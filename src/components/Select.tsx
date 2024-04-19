f7cbbb21a25743ef9663fe67441c58deba29e3d7
John <jng9230@gmail.com>
2024-04-18 22:30:36 -0400

    checkbox finished

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -13,61 +13,99 @@ export default function Select({
     isMultiSelect?: boolean
 
 }) {
-    const [selectedContent, setSelectedContent] = useState<string[]>([])
+    const [selectedContent, setSelectedContent] = useState<boolean[]>(Array(options.length).fill(false))
     isMultiSelect = isMultiSelect ? isMultiSelect : false
-    console.log(isMultiSelect)
 
-    const handleSelectClick = (d: string) => {
-        console.log(`selected ${d}`)
-        setSelectedContent([d, ...selectedContent])
-        // TODO: toggle the checkbox
+    const handleSelectClick = (d: string, i: number) => {
+        toggleOption(i)
+    }
+
+    const handleDeselect = (i: number) => {
+        toggleOption(i)
+    }
+
+    const toggleOption = (i: number) => {
+        setSelectedContent((selectedContent) => {
+            const temp = [...selectedContent]
+            temp[i] = !selectedContent[i]
+            return temp
+        })
+    }
+
+    const [showOptions, setShowOptions] = useState(false)
+    const handleShowOptions = () => setShowOptions(true);
+    const toggleShowOptions = () => setShowOptions(showOptions => !showOptions);
+    // const handleHideOptions = (e: React.FocusEvent) => {
+    const handleHideOptions = () => {
+        // console.log(e.target);
+        // console.log(e.target.getAttribute('type'));
+
+        // if (e.target.getAttribute('type') === "checkbox") { return; }
+        console.log("blurring");
+        setShowOptions(false);
     }
 
     return (
-        <div className={"flex flex-col w-80 border-black border-2 rounded-md p-2 space-y-2"}>
-            {
-                !isMultiSelect ?
-                    <>
-                        <label htmlFor="" className=""> {label} </label>
-                        <select name="" id="" className="border-2 border-black">
-                            <option value=""> </option>
-                            {
-                                options.map((d, i) => {
-                                    return <option value={d} key={i}> {d} </option>
-                                })
-                            }
-                        </select>
-                    </>
-                    :
-                    <>
-                        <label htmlFor=""> {label} </label>
-                        <ul className="border-2 border-black p-2 max-h-20 overflow-y-auto">
-                            {
-                                selectedContent.map((d, i) => {
+        <div className={"flex flex-col w-80 border-black border-2 rounded-md p-2 space-y-2"}
+            // onMouseDown={(e) => e.preventDefault()}
+            // onBlur={e => handleHideOptions(e)}
+            // onMouseLeave={e => handleHideOptions()}
+            onBlur={handleHideOptions}
+        >
+            {!isMultiSelect ?
+                <>
+                    <label htmlFor="" className=""> {label} </label>
+                    <select name="" id="" className="border-2 border-black">
+                        <option value=""> </option>
+                        {
+                            options.map((d, i) => {
+                                return <option value={d} key={i}> {d} </option>
+                            })
+                        }
+                    </select>
+                </>
+                :
+                <>
+                    <label htmlFor=""> {label} </label>
+                    <ul className="border-2 border-black p-2 max-h-20 overflow-y-auto cursor-pointer"
+                        onClick={handleShowOptions}
+                    >
+                        {
+                            // [...selectedContent].map((d, i) => {
+                            [...selectedContent].map((d, i) => {
+                                if (d) {
                                     return (
-                                        <li key={i} className="inline-flex items-center border-2 border-gray-300 m-1">
-                                            <span>{d}</span>
-                                            <BiX />
+                                        <li key={i} className="inline-flex items-center border-2 border-gray-300 m-1 px-2 py-1 justify-between">
+                                            <div className="w-4/5 text-ellipsis overflow-hidden">{options[i]}</div>
+                                            <BiX className="cursor-pointer relative top-[1px] hover:text-red-600 text-lg"
+                                                onClick={() => { handleDeselect(i) }}
+                                            />
                                         </li>
                                     )
+                                } else {
+                                    return <span key={i}></span>
+                                }
+                            })
+                        }
+                    </ul>
+                    <div className={`border-2 border-black ${!showOptions ? "hidden" : ""}`}
+                    >
+                        <ul className="max-h-60 overflow-y-auto">
+                            {
+                                options.map((d, i) => {
+                                    // const checked = selectedContent.has(d);
+                                    const checked = selectedContent[i]
+                                    return <li key={i} value={d}
+                                        className="flex items-center px-2 py-1"
+                                    >
+                                        <Checkbox checked={checked} onChange={() => { handleSelectClick(d, i) }} />
+                                        {d}
+                                    </li>
                                 })
                             }
                         </ul>
-                        <div className="border-2 border-black" >
-                            <ul className="max-h-60 overflow-y-auto">
-                                {
-                                    options.map((d, i) => {
-                                        return <li key={i} value={d}
-                                            className="hover:bg-gray-500 cursor-pointer flex items-center px-2 py-1"
-                                        >
-                                            <Checkbox checked={false} onChange={() => { handleSelectClick(d) }} />
-                                            {d}
-                                        </li>
-                                    })
-                                }
-                            </ul>
-                        </div>
-                    </>
+                    </div>
+                </>
             }
         </div >
     )
